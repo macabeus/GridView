2e57d98051cd1b3a2876ed42b457832384be182e
Bruno Macabeus Aquino <bruno.macabeus@gmail.com>
2017-06-01 08:17:32 -0300

    test: add some tests for GridConfiguration

--- GridView/GridViewTests/GridViewTests.swift
+++ GridView/GridViewTests/GridViewTests.swift
@@ -10,16 +10,79 @@ import Quick
 import Nimble
 import GridView
 
+class CellBlue: SlotableCell {
+    
+    static let slotWidth = 1
+    static let slotHeight = 1
+    var params: [String : Any] = [:]
+    
+    func load(params: [String: Any]) {
+        
+    }
+}
+
+extension GridConfiguration.ParseSlotStep: Equatable {
+    
+    static func ==(lhs: GridConfiguration.ParseSlotStep, rhs: GridConfiguration.ParseSlotStep) -> Bool {
+        if case .cell(row: let rowLhs, collumn: let columnLhs) = lhs,
+            case .cell(row: let rowRhs, collumn: let columnRhs) = rhs {
+            
+            return (rowLhs == rowRhs) && (columnLhs == columnRhs)
+        } else if case .newRow() = lhs, case .newRow() = rhs {
+            
+            return true
+        } else {
+            
+            return false
+        }
+    }
+}
 
 class GridConfigurationTests: QuickSpec {
     override func spec() {
         
-        let slots: [[Slot]] = [[]]
-        let gridConfiguration = GridConfiguration(slots: slots)
-        
         describe("a gridConfiguration") {
-            it("is loud") {
-                expect(gridConfiguration.slots.count).to(equal(1))
+            describe("empty grid") {
+                let emptyGridConfiguration = GridConfiguration(slots: [[]])
+                
+                it("check total of rows") {
+                    expect(emptyGridConfiguration.gridNumberOfRows()).to(equal(1))
+                }
+
+                it("check total of columns") {
+                    expect(emptyGridConfiguration.gridNumberOfColumns()).to(equal(0))
+                }
+            }
+            
+            describe("grid with one row and tree columns") {
+                let filledGridConfiguration = GridConfiguration(slots:
+                    [
+                        [Slot(cell: CellBlue.self, params: [:]), Slot(cell: CellBlue.self, params: [:]), Slot(cell: CellBlue.self, params: [:])]
+                    ]
+                )
+                
+                it("check total of rows") {
+                    expect(filledGridConfiguration.gridNumberOfRows()).to(equal(1))
+                }
+                
+                it("check total of columns") {
+                    expect(filledGridConfiguration.gridNumberOfColumns()).to(equal(3))
+                }
+
+                it("check parse slots") {
+                    let testParseSlots = filledGridConfiguration.parseSlots()
+                    
+                    let mockParseSlots: [GridConfiguration.ParseSlotStep] = [
+                        .cell(row: 0, collumn: 0), .cell(row: 0, collumn: 1), .cell(row: 0, collumn: 2), .newRow()
+                    ]
+                    
+                    expect(testParseSlots.count).to(equal(mockParseSlots.count))
+                    
+                    for i in zip(testParseSlots, mockParseSlots) {
+                        
+                        expect(i.0).to(equal(i.1))
+                    }
+                }
             }
         }
     }
